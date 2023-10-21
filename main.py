a91aca519591a64f12a60ccd6cbd743f46b6b7a3
Octavian <octavian.hagi-memet@edu.sait.ca>
2023-10-21 13:53:39 -0600

    Step 2: -changed file path from previous repo to new     - local txt file makes it easier to call, it was in augusts "wed" repo -used for loop to iterate within split of data — the read file -used isalpha() and isdigit() to count letters/numbers respectively     -added a count for each letter and number in the characters of the Lines -I tried doing this with a dictionary and found myself in a world of pain     - this solution is mucher simpler and worked for me.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-with open("Lucky/wed/mydefaults.ini.txt") as ini_file:
+# context manager
+with open("mydefaults.ini.txt") as ini_file:
     data = ini_file.read()
 
+# initialization
 letters = 0
 numbers = 0
+
+for line in data.split("\n"):
+    for char in line:
+        if char.isalpha():
+            letters += 1
+        elif char.isdigit():
+            numbers += 1
+
+print(f"letter count: {letters}")
+print(f"Number count: {numbers}")
